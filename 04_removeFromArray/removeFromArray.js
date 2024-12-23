5ee0ca1ddc6c2e76c7ece676f61593b1a459c44a
IammHardy <iamhardyusuf@gmail.com>
2024-12-23 08:39:23 +0100

    All files has been updated and assignments have been completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...value) {
+const newArray = []
+
+    arr.forEach((item) => {
+        if(!value.includes(item)){
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
